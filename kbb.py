41d7490e2b34f54667e00ced9630c234cca4969c
Urja Rannikko <urjaman@gmail.com>
2021-07-25 14:23:29 +0300

    first one that actually builds something
    (also, ignore more stuff)

--- kbb.py
+++ kbb.py
@@ -9,6 +9,9 @@ import urllib.request
 import urllib.error
 import shutil
 import subprocess
+import time
+import datetime
+from subprocess import DEVNULL, PIPE, STDOUT
 
 
 def json_readf(fn):
@@ -17,12 +20,22 @@ def json_readf(fn):
     return json.loads(d)
 
 
-def subX(*args, **kwargs):
-    return subprocess.run(*args, **kwargs).returncode == 0
-
-
 def sub(*args, **kwargs):
-    c = subprocess.run(*args, **kwargs).returncode
+    c = subprocess.run(*args, **kwargs)
+    if c.returncode != 0:
+        return False
+    if c.stdout:
+        return c.stdout
+    return True
+
+
+def subc(*args, **kwargs):
+    c = subprocess.run(*args, **kwargs)
+    if c.returncode != 0:
+        print("subprocess failed: ", args)
+        print("code:", c)
+        sys.exit(1)
+    return c.stdout
 
 
 url = "https://www.kernel.org/releases.json"
@@ -57,7 +70,7 @@ for r in rels["releases"]:
                 continue
             found = True
             if r["version"] != o["version"]:
-                update_mainline = True
+                update_mainline = r["version"]
             break
         if o["moniker"] == "mainline":
             continue
@@ -69,37 +82,121 @@ for r in rels["releases"]:
             break
     if not found:
         if r["moniker"] == "mainline":
-            update_mainline = True
+            update_mainline = r["version"]
         else:
             update_stable.append(r["version"])
 
 print("Update stable:", bool(update_stable), update_stable)
-print("Update mainline:", update_mainline)
+print("Update mainline:", bool(update_mainline), update_mainline)
 
 if update_stable or update_mainline:
     os.chdir("linux")
     if update_mainline:
         print("Fetching mainline")
-        if not sub(["git", "fetch", "mainline", "master", "--tags"]):
-            sys.exit(1)
+        subc(["git", "fetch", "mainline", "master", "--tags"])
         print("Done")
 
     if update_stable:
         print("Fetching stable(s)")
         targets = ["tags/v" + x for x in update_stable]
-        if not sub(["git", "fetch", "stable"] + targets):
-            sys.exit(1)
+        subc(["git", "fetch", "stable"] + targets)
         print("Done")
     os.chdir("..")
 
-kernel_c201 = {
-    "dir": "linux",
+kernel_c201ml = {
+    "dir": "linux-kbb-c201",
+    "patchset": "c201",
     "patches": 14,
     "build": "./makepkg-c201-test.sh",
-    "branch": "c201-{}_v1",
-    "branchfile": "c201-test/git-branch",
+    "verpolicy": "mainline",
 }
+
+# Calling rebase and tag "repatch" ... ok.
+def repatch_indir(patchset, patches, newver):
+    kbbpatchcnt = ".kbb_patchcount"
+
+    tagname = patchset + "-" + newver
+    if os.path.exists(".skipkbb"):
+        return False
+
+    if sub(
+        ["git", "rev-parse", "refs/tags/" + tagname], stdout=DEVNULL, stderr=DEVNULL
+    ):
+        return tagname
+
+    if os.path.exists(kbbpatchcnt):
+        with open(kbbpatchcnt) as f:
+            patches = int(f.read())
+
+    vertag = "tags/v" + newver
+
+    if not sub(["git", "rebase", "HEAD~" + str(patches), "--onto", vertag]):
+        print(
+            "Finish rebase manually. Use 'touch .skipkbb' to skip this kernel for now instead."
+        )
+        sub(["/bin/bash"])
+
+    if os.path.exists(".skipkbb"):
+        return False
+
+    count = subc(["git", "rev-list", "--count", vertag + "..HEAD"], stdout=PIPE)
+    count = int(count.decode())
+    if count != patches:
+        print(
+            f"Warning: patchset {patchset} now (version {newver}) has {count} patches!"
+        )
+        with open(kbbpatchcnt, "w") as f:
+            f.write(str(count))
+
+    subc(["git", "tag", tagname])
+    return tagname
+
+
+def repatch(dir, patchset, patches, newver):
+    os.chdir(dir)
+    r = repatch_indir(patchset, patches, newver)
+    os.chdir("..")
+    return r
 
 
 def doakernel(k):
-    pass
+    nv = None
+    vp = k["verpolicy"]
+    if vp == "mainline":
+        if update_mainline:
+            nv = update_mainline
+        else:
+            return
+    elif vp == "stable":
+        if rels["latest_stable"] in update_stable:
+            vp = rels["latest_stable"]
+        else:
+            return
+    else:
+        for v in update_stable:
+            if v.startswith(vp):
+                nv = v
+    if not nv:
+        return
+    kname = k["patchset"] + "-" + vp
+    print(f"Re/patching {kname} to version {nv}")
+    tag = repatch(k["dir"], k["patchset"], k["patches"], nv)
+    if not tag:
+        print("No tag returned - skipping build")
+        return
+    today = datetime.date.today()
+    ymd = today.strftime("%y%m%d")
+    pids = str(os.getpid())
+    logfn = "kbb-build-" + kname + "-" + ymd + "-v" + nv + " " + pids + ".log"
+    print(f"Building - for details see '{logfn}'")
+    with open(logfn, "w") as f:
+        if sub([k["build"], tag], stdin=DEVNULL, stderr=STDOUT, stdout=f):
+            print("Done. Return value zero (y).")
+        else:
+            print("Oopsie? Build ended with nonzero return value :(")
+
+
+doakernel(kernel_c201ml)
+
+# finally, move releases to prev
+os.replace(curr_fn, prev_releases)
--- makepkg-c201-test.sh
+++ makepkg-c201-test.sh
@@ -5,4 +5,7 @@ export ARCH="arm"
 export CROSS_COMPILE="arm-unknown-linux-gnueabihf-"
 export PATH=$PATH:$HOME/x-tools7h/arm-unknown-linux-gnueabihf/bin
 cd c201-test
+if [ -n "$1" ]; then
+	echo $1 > git-tag
+fi
 makepkg --config ../armv7h-makepkg.conf
