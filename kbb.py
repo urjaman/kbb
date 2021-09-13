4a720dd6581a1c427342cc06e1646c440a22bcc3
Urja Rannikko <urjaman@gmail.com>
2021-09-13 09:51:42 +0300

    more code reorganization

--- kbb.py
+++ kbb.py
@@ -18,6 +18,22 @@ from subprocess import DEVNULL, PIPE, STDOUT
 from email.message import EmailMessage
 
 
+kernel_c201ml = {
+    "dir": "linux-kbb-c201",
+    "patchset": "c201",
+    "build": "./makepkg-c201-test.sh",
+    "verpolicy": "mainline",
+}
+
+kernel_c201_stable = {
+    "dir": "linux-kbb-c201-stable",
+    "patchset": "c201",
+    "build": "./makepkg-c201-stable.sh",
+    "verpolicy": "stable",
+}
+
+kernels = [kernel_c201ml, kernel_c201_stable]
+
 url = "https://www.kernel.org/releases.json"
 
 prev_releases = "prev_releases.json"
@@ -168,35 +184,32 @@ def repatch(dir, patchset, newver, verpolicy):
     return r
 
 
-successlist = []
 
-def doakernel(k):
-    global successlist
-
+def doakernel(k, rels, update_mainline, update_stable):
     nv = None
     vp = k["verpolicy"]
     if vp == "mainline":
         if update_mainline:
             nv = update_mainline
         else:
-            return
+            return None
     elif vp == "stable":
         if rels["latest_stable"]["version"] in update_stable:
             nv = rels["latest_stable"]["version"]
         else:
-            return
+            return None
     else:
         for v in update_stable:
             if v.startswith(vp):
                 nv = v
     if not nv:
-        return
+        return None
     kname = k["patchset"] + "-" + vp
     print(f"Re/patching {kname} to version {nv}")
     tag = repatch(k["dir"], k["patchset"], nv, vp)
     if not tag:
         print("No tag returned - skipping build")
-        return
+        return None
     today = datetime.date.today()
     ymd = today.strftime("%y%m%d")
     pids = str(os.getpid())
@@ -205,100 +218,94 @@ def doakernel(k):
     with open(logfn, "w") as f:
         if sub([k["build"], tag], stdin=DEVNULL, stderr=STDOUT, stdout=f):
             print("Done. Return value zero (y).")
-            successlist.append(f"{kname} {nv}")
+            return f"{kname} {nv}"
         else:
             print("Oopsie? Build ended with nonzero return value :(")
             mail(f"Build failure {kname} {nv}", logfn)
             with open("ATTN.txt", "a") as of:
                 of.write(logfn + "\n")
+            return None
 
 
-kernel_c201ml = {
-    "dir": "linux-kbb-c201",
-    "patchset": "c201",
-    "build": "./makepkg-c201-test.sh",
-    "verpolicy": "mainline",
-}
 
-kernel_c201_stable = {
-    "dir": "linux-kbb-c201-stable",
-    "patchset": "c201",
-    "build": "./makepkg-c201-stable.sh",
-    "verpolicy": "stable",
-}
+def update_and_build():
+    if not os.path.exists(curr_fn):
+        r = urllib.request.urlopen(url, timeout=30)
+        with open(curr_fn, "w+b") as t:
+            shutil.copyfileobj(r, t)
 
+    if not os.path.exists(prev_releases):
+        os.rename(curr_fn, prev_releases)
+        sys.exit(0)
 
-if not os.path.exists(curr_fn):
-    r = urllib.request.urlopen(url, timeout=30)
-    with open(curr_fn, "w+b") as t:
-        shutil.copyfileobj(r, t)
+    rels = json_readf(curr_fn)
+    prevs = json_readf(prev_releases)
 
-if not os.path.exists(prev_releases):
-    os.rename(curr_fn, prev_releases)
-    sys.exit(0)
-
-rels = json_readf(curr_fn)
-prevs = json_readf(prev_releases)
-
-update_stable = []
-update_mainline = False
-for r in rels["releases"]:
-    if not r["version"][0].isdigit():
-        continue
-    verno = versplit(r["version"])
-    print(r["moniker"], verno)
-    found = False
-    for o in prevs["releases"]:
-        # mainline is just compared to mainline, rest to matching 2 numbers of version
-        if r["moniker"] == "mainline":
-            if o["moniker"] != "mainline":
-                continue
-            found = True
-            if r["version"] != o["version"]:
-                update_mainline = r["version"]
-            break
-        if o["moniker"] == "mainline":
+    update_stable = []
+    update_mainline = False
+    for r in rels["releases"]:
+        if not r["version"][0].isdigit():
             continue
-        oldver = versplit(o["version"])
-        if oldver[0:2] == verno[0:2]:
-            found = True
-            if o["version"] != r["version"]:
+        verno = versplit(r["version"])
+        print(r["moniker"], verno)
+        found = False
+        for o in prevs["releases"]:
+            # mainline is just compared to mainline, rest to matching 2 numbers of version
+            if r["moniker"] == "mainline":
+                if o["moniker"] != "mainline":
+                    continue
+                found = True
+                if r["version"] != o["version"]:
+                    update_mainline = r["version"]
+                break
+            if o["moniker"] == "mainline":
+                continue
+            oldver = versplit(o["version"])
+            if oldver[0:2] == verno[0:2]:
+                found = True
+                if o["version"] != r["version"]:
+                    update_stable.append(r["version"])
+                break
+        if not found:
+            if r["moniker"] == "mainline":
+                update_mainline = r["version"]
+            else:
                 update_stable.append(r["version"])
-            break
-    if not found:
-        if r["moniker"] == "mainline":
-            update_mainline = r["version"]
+
+    print("Update stable:", bool(update_stable), update_stable)
+    print("Update mainline:", bool(update_mainline), update_mainline)
+
+    if update_stable or update_mainline:
+        os.chdir("linux")
+        if update_mainline:
+            print("Fetching mainline")
+            subc(["git", "fetch", "mainline", "master", "--tags"])
+            print("Done")
+
+        if update_stable:
+            print("Fetching stable(s)")
+            targets = ["tags/v" + x for x in update_stable]
+            subc(["git", "fetch", "--tags", "stable"] + targets)
+            print("Done")
+        os.chdir("..")
+
+
+
+    successlist = []
+    for k in kernels:
+        r = doakernel(k, rels, update_mainline, update_stable)
+        if r:
+            succeslist.append(r)
+
+    # finally, move releases to prev
+    os.replace(curr_fn, prev_releases)
+
+    if successlist:
+        if len(successlist) > 1:
+            mail(f"Success building {len(successlist)} kernels", log="\n".join(successlist) + "\n")
         else:
-            update_stable.append(r["version"])
-
-print("Update stable:", bool(update_stable), update_stable)
-print("Update mainline:", bool(update_mainline), update_mainline)
-
-if update_stable or update_mainline:
-    os.chdir("linux")
-    if update_mainline:
-        print("Fetching mainline")
-        subc(["git", "fetch", "mainline", "master", "--tags"])
-        print("Done")
-
-    if update_stable:
-        print("Fetching stable(s)")
-        targets = ["tags/v" + x for x in update_stable]
-        subc(["git", "fetch", "--tags", "stable"] + targets)
-        print("Done")
-    os.chdir("..")
+            mail("Success building " + successlist[0], log="\n")
 
 
 
-
-doakernel(kernel_c201ml)
-doakernel(kernel_c201_stable)
-
-# finally, move releases to prev
-os.replace(curr_fn, prev_releases)
-
-if successlist:
-    if len(successlist) > 1:
-        mail(f"Success building {len(successlist)} kernels", log="\n".join(successlist) + "\n")
-    else:
-        mail("Success building " + successlist[0], log="\n")
+update_and_build()
