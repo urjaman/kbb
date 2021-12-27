5efc37b10a32bbf5b820dfe7b2c35d592127f6ad
Urja Rannikko <urjaman@gmail.com>
2021-12-27 06:56:10 +0200

    Updates to allow building -chromebook packages for C201 (+other)
    (most of it is things installed on the host, but ehh...)
    also: other junk, as always

--- kbbcron.sh
+++ kbbcron.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-screen -h 1000 -L -Logfile "log/cron-$(date +%y%m%d)-$$.log" -dmS kbbcron -t kbb flock -n kbb.lock ./kbb.py
+screen -h 1000 -L -Logfile "log/cron-$(date +%y%m%d-%H%M%S).log" -dmS kbbcron -t kbb flock -n kbb.lock ./kbb.py
--- repothis.sh
+++ repothis.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-[ $# == 2 ] || exit 1
-# Usage: repothis.sh arch package
+[ $# -ge 2 ] || exit 1
+# Usage: repothis.sh arch package [package..]
 mkdir -p kbbpkgs-mp pkgarchive
 sshfs urja.dev:srv/kbbpkgs kbbpkgs-mp
-cp "$2" "kbbpkgs-mp/$1/"
-mv "$2" pkgarchive/
-PKG=$(basename $2)
-cd "kbbpkgs-mp/$1/"
-repo-add -R kbbpkgs.db.tar.gz "$PKG"
-cd ../..
+PARCH="$1"
+shift 1
+while [ -n "$1" ]; do
+	cp "$1" "kbbpkgs-mp/$PARCH/"
+	mv "$1" pkgarchive/
+	PKG=$(basename $1)
+	cd "kbbpkgs-mp/$PARCH/"
+	repo-add -R kbbpkgs.db.tar.gz "$PKG"
+	cd ../..
+	shift 1
+done
 fusermount -u kbbpkgs-mp
 exit 0
