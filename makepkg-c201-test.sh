7222e52e8235442719340e3532e70db71607e9ce
Urja Rannikko <urjaman@gmail.com>
2022-07-30 01:03:45 +0300

    reworking c201 mainline build

--- makepkg-c201-test.sh
+++ makepkg-c201-test.sh
@@ -5,9 +5,6 @@ export ARCH="arm"
 export CROSS_COMPILE="arm-unknown-linux-gnueabihf-"
 export PATH=$PATH:$HOME/x-tools7h/arm-unknown-linux-gnueabihf/bin
 cd c201-test
-if [ -n "$1" ]; then
-	echo $1 > git-tag
-fi
 makepkg --config ../armv7h-makepkg.conf
 cd ..
 ./repothis.sh armv7h c201-test/*.pkg.tar.*
