d9c9f0bd61ba5ddbb63a61e1fe75f8dd9594a4e6
Urja Rannikko <urjaman@gmail.com>
2023-12-26 19:16:09 +0200

    Did a -test build of c201-6.6.8
    (Spoiler: it f'ing runs.)

--- makepkg-c201-test.sh
+++ makepkg-c201-test.sh
@@ -6,6 +6,6 @@ export CROSS_COMPILE="arm-unknown-linux-gnueabihf-"
 export PATH=$PATH:$HOME/x-tools7h/arm-unknown-linux-gnueabihf/bin
 export MAKEPKG_CONF="$(pwd -P)/armv7h-makepkg.conf"
 cd c201-test
-makepkg
+makepkg -s
 cd ..
 ./repothis.sh armv7h c201-test/*.pkg.tar.*
